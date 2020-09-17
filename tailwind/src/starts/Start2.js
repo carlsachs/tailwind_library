4476e30778ed0f8eac2317a5b70728aef1c705ad
carlsachs <sachscarl@gmail.com>
2020-09-16 23:11:52 -0400

    more progress on start page 1

--- tailwind/src/starts/Start2.js
+++ tailwind/src/starts/Start2.js
@@ -18,7 +18,9 @@ const Img = tw.img`absolute inset-0 w-full h-full object-cover object-center z-0
 const SubColumnOne = tw.div`bg-blue-900 h-full`;
 const SubColumnTwo = tw.div`bg-blue-700 h-full`;
 const SubColumnThree = tw.div`bg-blue-500 h-full`;
-const BrandText = tw.text`text-6xl mt-24 text-blue-500`;
+const BrandText = tw.text`text-6xl mt-24 text-blue-600`;
+const BrandDes = tw.text`text-4xl py-8 px-8 text-blue-200`;
+const BrandDiv = tw.div`flex justify-center mt-20`;
 
 export default () => {
 
@@ -32,7 +34,11 @@ export default () => {
                         <BrandText>Cyphen</BrandText>
                     </CyphenDiv>
                     <CyphenDescription>
-                    
+                        <BrandDiv>
+                        <BrandDes>
+                            The <span className="text-blue-500">Professional</span> Look <br></br><span className="text-blue-500">You Deserve</span>
+                        </BrandDes>
+                        </BrandDiv>
                     </CyphenDescription>
                 </ColumnOne>
                 <ColumnTwo>
